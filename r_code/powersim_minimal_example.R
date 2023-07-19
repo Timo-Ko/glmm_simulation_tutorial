eee50c331afb8508bbcdeeb2b5c41c1e56bd802b
Florian Pargent <florian.pargent@yahoo.com>
2023-07-19 16:37:54 +0200

    mini update to the model structure and data simulation

--- r_code/powersim_minimal_example.R
+++ r_code/powersim_minimal_example.R
@@ -7,10 +7,10 @@ library(furrr)
 
 # simulate a dataset based on population parameters
 simulate <- function(n_subjects = 100, n_items = 50,
-  b_0 = 0, b_e = 0.2, b_a = 0.2, sd_u0s = 1, sd_u0i = 1, ...){
+  b_0 = 0, b_e = 0.2, b_a = 0.2, b_ea = 0.1, sd_u0s = 1, sd_u0i = 1, ...){
   # simulate design
   dat <- add_random(subject = n_subjects, item = n_items) %>%
-    add_between("subject", expert = c(1, 0), .prob = 0.5) %>%
+    add_between("subject", expert = c(1, 0), .prob = 0.8) %>%
     add_between("subject", advice = c(1, 0)) %>%
     
     # add_between("subject", interpretable = c(1, 0)) %>%
@@ -20,7 +20,8 @@ simulate <- function(n_subjects = 100, n_items = 50,
     add_ranef("subject", u0s = sd_u0s) %>%
     add_ranef("item", u0i = sd_u0s) %>%
     # compute dependent variable
-    mutate(linpred = b_0 + b_e * expert + b_a * advice + u0i + u0s) %>%
+    mutate(linpred = b_0 + b_e * expert + b_a * advice + b_ea * expert * advice +
+        u0i + u0s) %>%
     mutate(y_prob = plogis(linpred)) %>%
     mutate(y = rbinom(n = n(), size = 1, prob = y_prob))
   dat
@@ -28,7 +29,7 @@ simulate <- function(n_subjects = 100, n_items = 50,
 
 # fit a model to a data set and compute a pvalue
 analyse <- function(data, 
-  formula_chr = "y ~ 1 + expert + advice + (1|subject) + (1|item)", 
+  formula_chr = "y ~ 1 + expert * advice + (1|subject) + (1|item)", 
   null_hypothesis = "expert <= 0", ...){
   # fit model
   model <- glmer(as.formula(formula_chr), data = data, family = "binomial")
