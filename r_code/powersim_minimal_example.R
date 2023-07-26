3e0b99715640db5fab8919319ff3393718daf26e
Florian Pargent <florian.pargent@yahoo.com>
2023-07-26 10:45:25 +0200

    update minimal example

--- r_code/powersim_minimal_example.R
+++ r_code/powersim_minimal_example.R
@@ -7,29 +7,26 @@ library(furrr)
 
 # simulate a dataset based on population parameters
 simulate <- function(n_subjects = 100, n_items = 50,
-  b_0 = 0, b_e = 0.2, b_a = 0.2, b_ea = 0.1, sd_u0s = 1, sd_u0i = 1, ...){
+  b_0 = 0, b_e = 0.2, b_c = 0.2, b_ec = 0.1, sd_u0s = 1, sd_u0i = 1, ...){
   # simulate design
   dat <- add_random(subject = n_subjects, item = n_items) %>%
     add_between("subject", expert = c(1, 0), .prob = 0.8) %>%
-    add_between("subject", advice = c(1, 0)) %>%
-    
-    # add_between("subject", interpretable = c(1, 0)) %>%
-    # mutate(interpretable = replace(interpretable, advice == 1, NA)) %>%
-    
+    mutate(advice_present = rbinom(n(), 1, prob = 2/3)) %>%
+    mutate(advice_correct = if_else(advice_present == 1, rbinom(n(), 1, prob = 0.8), 0)) %>%
     # add random effects
     add_ranef("subject", u0s = sd_u0s) %>%
     add_ranef("item", u0i = sd_u0s) %>%
     # compute dependent variable
-    mutate(linpred = b_0 + b_e * expert + b_a * advice + b_ea * expert * advice +
-        u0i + u0s) %>%
+    mutate(linpred = b_0 + b_e * expert + b_c * advice_correct + 
+        b_ec * expert * advice_correct + u0i + u0s) %>%
     mutate(y_prob = plogis(linpred)) %>%
-    mutate(y = rbinom(n = n(), size = 1, prob = y_prob))
+    mutate(y_bin = rbinom(n = n(), size = 1, prob = y_prob))
   dat
 }
 
 # fit a model to a data set and compute a pvalue
 analyse <- function(data, 
-  formula_chr = "y ~ 1 + expert * advice + (1|subject) + (1|item)", 
+  formula_chr = "y_bin ~ 1 + expert + advice_present + advice_correct + expert:advice_correct + (1|subject) + (1|item)", 
   null_hypothesis = "expert <= 0", ...){
   # fit model
   model <- glmer(as.formula(formula_chr), data = data, family = "binomial")
